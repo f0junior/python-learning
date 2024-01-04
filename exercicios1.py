8118791a3b2becc9e2d0876a7a004124564350c6
Francisco José Machado Junior <machado.fjunior@gmail.com>
2024-01-04 13:05:16 +0000

    Primeiro exercício proposto no curso introdutório do Curso em Vídeos

---
+++ exercicios1.py
@@ -0,0 +1,14 @@
+nome = input("Olá, Com quem estou falando: ")
+print("É um prazer conhecê-lo", nome)
+
+dia = input("Por gentileza, informe seu dia de nascimento: ")
+mes = input("Agora preciso que informe o mês: ")
+ano = input("Por último o ano: ")
+
+print(dia, '/', mes, '/', ano, "essa é sua data de nascimento formatada.")
+
+print("Por último iremos fazer uma conta de soma.")
+primeiroNumero = input("Por gentileza, informe um número: ")
+segundoNumero = input("Agora outro número: ")
+
+print("A soma desses dois números é:", int(primeiroNumero) + int(segundoNumero))
