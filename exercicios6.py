7b8692501ca91a93b6020ebc2ec19151f8edae64
Francisco José Machado Junior <machado.fjunior@gmail.com>
2024-01-31 22:32:17 +0000

    Resolucao do exercicio 6

---
+++ exercicios6.py
@@ -0,0 +1,5 @@
+while True:
+    numero = int(input("Informe um número inteiro: "))
+    print("O dobro de {} é {}".format(numero, numero * 2))
+    print("O triplo de {} é {}".format(numero, numero * 3))
+    print("A raiz quadrada de {} é {:.2f}".format(numero, numero ** 0.5))
