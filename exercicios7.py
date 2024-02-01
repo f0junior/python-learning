8281752f062b4188ba9b01f510c68c2c7564f0f7
Francisco José Machado Junior <machado.fjunior@gmail.com>
2024-02-01 22:34:12 +0000

    RResolucao do exercicio 7

---
+++ exercicios7.py
@@ -0,0 +1,15 @@
+while True:
+    print("Vamos calcular a média aritmética.")
+
+    maisNumero = "s"
+    numeros = []
+    while maisNumero == "s" or maisNumero == "S":
+        numeros.append(int(input("Informe um númmero inteiro: ")))
+
+        maisNumero = input(
+            "Deseja informar mais um número (s ou S para Sim, qualquer outro valor para não): "
+        )
+
+    print("A média aritmética dos números: ", end="")
+    print(*numeros, sep=", ", end="")
+    print(" é {}.".format(sum(numeros) / len(numeros)))
