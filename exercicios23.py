197a60d5223d63a842c0ef7be63e165d42489164
Francisco José Machado Junior <machado.fjunior@gmail.com>
2024-05-01 18:44:52 +0000

    Resolucao do exercicio 23

---
+++ exercicios23.py
@@ -0,0 +1,23 @@
+while True:
+    numero = input("Informe um número entre 0 e 9999: ")
+    if len(numero) > 4:
+        print("Número maior que o permitido.")
+        continue
+
+    print("Analisando o número {}.".format(numero))
+    # for index, value in enumerate(numero):
+    #     match (index):
+    #         case 0:
+    #             print("Unidade: {}".format(value))
+    #         case 1:
+    #             print("Dezena: {}".format(value))
+    #         case 2:
+    #             print("Centena: {}".format(value))
+    #         case 3:
+    #             print("Milhar: {}".format(value))
+
+    numero = int(numero)
+    print('Unidade: {}'.format(numero % 10))
+    print('Dezena: {}'.format(numero // 10 % 10))
+    print('Centena: {}'.format(numero // 100 % 10))
+    print('Milhar: {}'.format(numero // 1000 % 10))
