09e717dc086be7a50f8ad8c86dbbb5a511afdd56
Francisco José Machado Junior <machado.fjunior@gmail.com>
2024-05-28 18:56:02 +0000

    Resolucao do exercicio 46

---
+++ exercicios46.py
@@ -0,0 +1,7 @@
+from time import sleep
+
+print("Contagem para queima de fogos.")
+for i in range(10, -1, -1):
+    print(i)
+    sleep(1)
+print("\U0001F386 \U0001F386 \U0001F386 \U0001F386 \U0001F386")
