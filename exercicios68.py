b16555e0f14017b3f1d51d511bd4bb7218bc1fbc
Francisco José Machado Junior <machado.fjunior@gmail.com>
2024-08-16 19:17:20 +0000

    Resolucao do exercicio 68

---
+++ exercicios68.py
@@ -0,0 +1,26 @@
+import random
+
+print("Vamos jogar Impar ou Par")
+
+vitorias = 0
+while True:
+    aleatorio = random.randint(1, 10)
+
+    numero = int(input("Escolha um valor: "))
+    impar_ou_par = input("Impar ou par? [I/P]").strip().upper()
+
+    soma = aleatorio + numero
+
+    print(f"Você jogou {numero} e o computador jogou {aleatorio}.")
+    print(f"Total igual a {soma}, deu ", end="")
+    result = 'Par' if soma % 2 == 0 else 'Impar'
+    print(f"{result}.")
+
+    if (soma % 2 == 0 and impar_ou_par == 'P') or (soma % 2 != 0 and impar_ou_par == 'I'):
+        vitorias += 1
+        print("Você ganhou, vamos mais uma.")
+    else:
+        print("Você perdeu.")
+        print(f"Game Over! Você ganhou {vitorias} vezes.")
+        break
+    
