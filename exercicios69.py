afd0160527f441cae379b58d057abafc44cb3c7a
Francisco José Machado Junior <machado.fjunior@gmail.com>
2024-08-27 19:17:32 +0000

    Correções do exercício 69

--- exercicios69.py
+++ exercicios69.py
@@ -11,13 +11,13 @@ while True:
         break
 
 maior_idade = homens = mulher_menor_vinte = 0
-for pessoa in pesssoas:
-    if pessoa.idade > 18:
+for pessoa in pessoas:
+    if pessoa["idade"] > 18:
         maior_idade += 1
 
-    if pessoa.sexo == "M":
+    if pessoa["sexo"] == "M":
         homens += 1
-    elif pessoa.idade < 20:
+    elif pessoa["idade"] < 20:
         mulher_menor_vinte += 1
 
 print(f"{maior_idade} pessoas tem mais de 18 anos.")
